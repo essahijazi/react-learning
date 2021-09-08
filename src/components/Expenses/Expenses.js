1fbe8bff90591752691974db0c413a173cf065c5
Essa Hijazi <essahijazi18@gmail.com>
2021-09-07 22:10:02 -0400

    List rendering + conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState("2019");
+  const [selectedYear, setSelectedYear] = useState("2022");
 
   const onFilterHandler = (selectedYear) => {
     setSelectedYear(selectedYear);
   };
+
+  const filteredByYearExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedYear
+  );
 
   return (
     <Card className="expenses">
@@ -17,27 +21,7 @@ const Expenses = (props) => {
         defaultYear={selectedYear}
         onFilterHandler={onFilterHandler}
       />
-
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      <ExpensesList expenses={filteredByYearExpenses} />
     </Card>
   );
 };
